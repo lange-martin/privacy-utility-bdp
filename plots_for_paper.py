a788f30b3732cd00b469f14d94e5473b919cc882
Martin Lange <contact@martin-lange.eu>
2024-11-08 15:09:28 +0100

    Add all relevant files.

---
+++ plots_for_paper.py
@@ -0,0 +1,209 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+COLORS = ["red", "green", "blue", "orange", "purple", "cyan"]
+MARKERS = ['x', '.', '+', '1']
+LINES = ['solid', 'dotted', 'dashed', 'dashdot', (0, (1, 10)), (0, (5, 10))]
+
+
+def plot_setup(max_y, min_y=0.0, max_x=1.0, min_x=0.0, x_label='ρ', y_label='BDPL', y_rot='vertical', x_scale='linear',
+               y_scale='linear', fontsize=20):
+    plt.xlabel(x_label, fontsize=fontsize)
+    plt.ylabel(y_label, fontsize=fontsize, rotation=y_rot)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    plt.yscale(y_scale)
+    plt.xscale(x_scale)
+    plt.ylim((min_y, max_y))
+    plt.xlim((min_x, max_x))
+
+
+# Return general bound for comparison with gaussian bound
+def gaussian_general_bound(n, b_minus_a=1, d_privacy_eps=1):
+    bdpls_old = n * b_minus_a * d_privacy_eps
+    return [0.0, 1.0], [bdpls_old, bdpls_old]
+
+
+# Return Gaussian bound
+def gaussian_new_bound(n, b_minus_a=1, d_privacy_eps=1):
+    max_rho = min(1.0, 1 / (n - 2))
+    rhos = np.linspace(0.0, max_rho, 1000)
+    bdpls = (((n / 2) ** 2 / ((1 / rhos) - n + 2)) + 1) * b_minus_a * d_privacy_eps
+
+    if n == 7:
+        plt.xticks(ticks=[0.0, 0.05, 0.1, 0.15, 0.2])
+    elif n == 5:
+        plt.xticks(ticks=[0.0, 0.1, 0.2, 0.3])
+
+    return rhos, bdpls
+
+
+# Plot Gaussian bound and general bound
+def gaussian_bounds(n, max_y, b_minus_a=1, d_privacy_eps=1, legend=False, save=False):
+    max_rho = 1.0 / (n - 2)
+    plot_setup(max_y, max_x=max_rho, x_label="ρ", y_label="BDPL", fontsize=25)
+
+    new_xs, new_ys = gaussian_new_bound(n, b_minus_a, d_privacy_eps)
+    plt.plot(new_xs, new_ys, color=COLORS[0], linestyle=LINES[0], linewidth=4, label='Gaussian bound')
+    old_xs, old_ys = gaussian_general_bound(n, b_minus_a, d_privacy_eps)
+    plt.plot(old_xs, old_ys, color=COLORS[1], linestyle=LINES[1], linewidth=4, label='General bound')
+
+    plt.grid(visible=True)
+    if legend:
+        plt.legend(fontsize=25)
+    if save:
+        plt.savefig(f'figures/gaussian_bound_n_{n}.pdf', bbox_inches='tight')
+    plt.show()
+
+
+def markov_bounds_new(max_y, legend=False, save=False):
+    plot_setup(max_y, min_y=1, max_x=5, y_scale='log', x_label='DP leakage', y_label='BDP leakage', fontsize=25)
+
+    gammas = [5, 10, 50, 100]
+    n = 100
+
+    epsilons = np.linspace(0, 5, 1000)
+    general_bound = epsilons * n
+    plt.plot(epsilons, general_bound, color=COLORS[0], linestyle=LINES[0], linewidth=4, label='General bound')
+
+    for i, gamma in enumerate(gammas):
+        markov_chain_bound = epsilons + 4 * np.log(gamma)
+        plt.plot(epsilons, markov_chain_bound, color=COLORS[i+1], linestyle=LINES[i+1], linewidth=4,
+                 label=f'Markov chain bound; γ = {gamma}')
+
+    plt.grid(visible=True)
+    if legend:
+        plt.legend(loc="upper center", bbox_to_anchor=(1.75, 1.0), fontsize=20)
+    if save:
+        plt.savefig(f'figures/markov_bounds_compared.pdf', bbox_inches='tight')
+    plt.show()
+
+
+# Minimum probability beta of a high error for a BDP algorithm for arbitrary data distribution
+def arbitrary_utility(save=False):
+    plot_setup(1.0, min_y=0.001, max_x=5.0, min_x=0, x_label='ε', y_label='β', y_scale='log', y_rot='horizontal')
+
+    bdp_epsilons = np.linspace(0.0, 5.0, 1000)
+    betas = 1.0 / (np.exp(bdp_epsilons) + 1)
+
+    plt.plot(bdp_epsilons, betas, color=COLORS[0], linewidth=4)
+    plt.grid(visible=True, which='both')
+    if save:
+        plt.savefig(f'figures/arbitrary_bound.pdf', bbox_inches='tight')
+    plt.show()
+
+
+# Markov chain bound and general bound compared
+def markov_bounds(ns, dp_epsilon):
+    plot_setup(max_y=45, max_x=100, min_x=1, x_label="ratio of maximum and minimum transition probability",
+               y_label="BDPL", x_scale="log")
+
+    ratios = np.linspace(1.0, 100.0, 1000)
+    new_bound = dp_epsilon + 4 * np.log(ratios)
+    plt.plot(ratios, new_bound)
+
+    for n in ns:
+        plt.plot([1, 100], [n * dp_epsilon, n * dp_epsilon], label=f"general bound for n = {n}")
+
+    plt.legend()
+    plt.show()
+
+
+# Values of n and ratio γ where the new Markov chain bound improves over general bound.
+def markov_new_bound_better(dp_epsilon, max_n=100, save=False):
+    plot_setup(max_y=10000, min_y=1, max_x=max_n, min_x=0, x_label='n', y_label='γ', y_scale='log', y_rot='horizontal')
+
+    ns = np.linspace(1, max_n, 1000)
+    alphas = np.exp(((ns - 1)/4) * dp_epsilon)
+
+    plt.fill_between(ns, alphas, facecolor='none', hatch='||', edgecolor=COLORS[0])
+
+    if save:
+        plt.savefig(f'figures/markov_bound_improvement_epsilon_{dp_epsilon}.pdf', bbox_inches='tight')
+    plt.show()
+
+
+# Maximum transition probability ratio γ so that Markov chain bound is only h times worse than DP privacy budget
+def markov_utility(dp_epsilons, save=False):
+    plot_setup(max_y=100, min_y=1, max_x=10, min_x=1, x_label='h', y_label='γ', y_rot='horizontal', y_scale='log')
+
+    hs = np.linspace(1, 10, 1000)
+    for i, dp_epsilon in enumerate(dp_epsilons):
+        gammas = np.exp(dp_epsilon * (hs - 1)/(4 * hs))
+        plt.plot(hs, gammas, linestyle=LINES[i], color=COLORS[i], label=f"ε = {dp_epsilon}")
+
+    plt.legend(fontsize=15, loc='upper right')
+    #plt.grid(visible=True, axis='y', which='both')
+    if save:
+        plt.savefig(f'figures/markov_utility_eps_{dp_epsilons}.pdf', bbox_inches='tight')
+    plt.show()
+
+def markov_utility_reverse_axes(dp_epsilons, save=False):
+    plot_setup(max_y=10, min_y=1, max_x=100, min_x=1, x_label='γ', y_label='h', y_rot='horizontal', x_scale='log')
+
+    gammas = np.logspace(0, 2, 1000)
+    for i, dp_epsilon in enumerate(dp_epsilons):
+        hs = 1. / (1. - (4 * np.log(gammas) / dp_epsilon))
+        hs = hs[hs >= 0]
+        plt.plot(gammas[:len(hs)], hs, linestyle=LINES[i], color=COLORS[i], label=f"ε = {dp_epsilon}")
+
+
+    plt.legend(fontsize=15, loc='upper right')
+    if save:
+        plt.savefig(f'figures/markov_utility_eps_{dp_epsilons}_reverse_axes.pdf', bbox_inches='tight')
+    plt.show()
+
+
+# alpha, beta accuracy of laplace algorithm for BDP for arbitrary correlation
+def arbitrary_laplace(ms, beta, save=False):
+    plot_setup(max_y=100, min_y=1, min_x=0, max_x=5, x_label='ε', y_label='α', y_scale='log', y_rot='horizontal')
+
+    epsilons = np.linspace(0, 5, 1000)
+
+    for i, m in enumerate(ms):
+        alphas = np.log(1 / beta) * (m / epsilons)
+        plt.plot(epsilons, alphas, linestyle=LINES[i], color='k', label=f"m = {m}")
+
+    plt.legend(fontsize=15)
+    if save:
+        plt.savefig(f'figures/arbitrary_alpha_beta_{beta}_m_{ms}.pdf', bbox_inches='tight')
+    plt.show()
+
+
+# Probability beta of a large error for any BDP algorithm for arbitrary correlation
+def arbitrary_prob_error_greater_than_half_dataset(save=False):
+    plot_setup(max_y=1, min_y=0, max_x=5, min_x=0, x_label='ε', y_label='β', y_rot='horizontal')
+    epsilons = np.linspace(0, 5, 1000)
+    betas = np.exp(-epsilons/2)
+    plt.plot(epsilons, betas, color=COLORS[0], linewidth=4)
+
+    plt.grid(visible=True)
+    if save:
+        plt.savefig(f'figures/arbitrary_counting_query_large_error.pdf', bbox_inches='tight')
+    plt.show()
+
+
+if __name__ == "__main__":
+    SAVE = False
+
+    markov_bounds_new(max_y=1000, legend=True, save=SAVE)
+
+    arbitrary_prob_error_greater_than_half_dataset(SAVE)
+    arbitrary_utility(save=SAVE)
+    #arbitrary_laplace([2, 5, 10, 100], beta=0.01)
+    #arbitrary_laplace([2, 5, 10, 100], beta=0.1)
+    #arbitrary_laplace([2, 5, 10, 100], beta=0.5)
+
+    gaussian_bounds(3, 10, legend=True, save=SAVE)
+    gaussian_bounds(5, 10, legend=False, save=SAVE)
+    gaussian_bounds(7, 10, save=SAVE)
+
+    markov_new_bound_better(dp_epsilon=0.5, save=SAVE)
+    markov_new_bound_better(dp_epsilon=1.0, save=SAVE)
+    markov_new_bound_better(dp_epsilon=2.0, save=SAVE)
+    #markov_bounds(ns=[5, 10, 15], dp_epsilon=3)
+
+    markov_utility(dp_epsilons=[0.5, 1, 2, 5, 10, 20], save=SAVE)
+    markov_utility_reverse_axes(dp_epsilons=[0.5, 1, 2, 5, 10, 20], save=SAVE)
+
+    # Utility experiment results are directly plotted in the utility_experiment.py file
---
+++ utility_experiment.py
@@ -0,0 +1,263 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from plots_for_paper import MARKERS, COLORS, LINES
+
+RNG = np.random.default_rng(seed=1253)
+
+
+# Here follow all queries we test, deterministic and made DP or BDP using different bounds
+
+def count_BB(datasets):
+    return np.sum((datasets == 2), axis=1)
+
+
+def count_active(datasets):
+    return np.sum((datasets > 0), axis=1)
+
+
+def sum(datasets):
+    return np.sum(datasets, axis=1)
+
+
+def laplace_mechanism(query, input_data, scale):
+    noise = RNG.laplace(0, scale, len(input_data))
+    return query(input_data) + noise, scale
+
+
+def count_BB_bdp_general_bound(datasets, epsilon, **kwargs):
+    m = kwargs['m']
+    return laplace_mechanism(count_BB, datasets, m / epsilon)
+
+
+def count_active_bdp_general_bound(datasets, epsilon, **kwargs):
+    n = datasets.shape[1]
+    return laplace_mechanism(count_active, datasets, n / epsilon)
+
+
+def count_active_bdp_markov_chain_boun(datasets, epsilon, **kwargs):
+    dp_eps = epsilon - 4 * np.log(np.max(kwargs['trans_probs'])/np.min(kwargs['trans_probs']))
+    if dp_eps > 0:
+        return laplace_mechanism(count_active, datasets, 1.0 / dp_eps)
+    else:
+        raise ValueError
+
+def count_active_dp(datasets, epsilon, **kwargs):
+    return laplace_mechanism(count_active, datasets, 1.0 / epsilon)
+
+
+def sum_bdp_general_bound(datasets, epsilon, **kwargs):
+    m = datasets.shape[1]
+    if 'm' in kwargs:
+        m = kwargs['m']
+    datasets = np.clip(datasets, kwargs['min_value'], kwargs['max_value'])
+    return laplace_mechanism(sum, datasets, m * (kwargs['max_value'] - kwargs['min_value']) / epsilon)
+
+
+def sum_bdp_gaussian_bound(datasets, epsilon, **kwargs):
+    n = datasets.shape[1]
+    datasets = np.clip(datasets, kwargs['min_value'], kwargs['max_value'])
+    bdp_factor = ((n**2)/(4*(1/kwargs['max_rho'] - n + 2)) + 1)*(kwargs['max_value'] - kwargs['min_value'])
+
+    dp_eps = epsilon / bdp_factor
+    return laplace_mechanism(sum, datasets, 1 / dp_eps)
+
+def sum_dp(datasets, epsilon, **kwargs):
+    datasets = np.clip(datasets, kwargs['min_value'], kwargs['max_value'])
+    return laplace_mechanism(sum, datasets, (kwargs['max_value'] - kwargs['min_value']) / epsilon)
+
+
+# Calculates the empirical transition probabilities of a Markov chain
+def calc_transition_probabilities(dataset):
+    inactive_to_inactive = 0
+    inactive_to_active = 0
+    active_to_inactive = 0
+    active_to_active = 0
+
+    prev_steps = 0
+    current_steps = 0
+
+    for index, row in dataset.iterrows():
+        if index == 0:
+            prev_steps = row['steps']
+            continue
+
+        current_steps = row['steps']
+        if prev_steps > 0:
+            if current_steps > 0:
+                active_to_active += 1
+            else:
+                active_to_inactive += 1
+        else:
+            if current_steps > 0:
+                inactive_to_active += 1
+            else:
+                inactive_to_inactive += 1
+
+        prev_steps = current_steps
+
+    times_inactive = inactive_to_inactive + inactive_to_active
+    times_active = active_to_active + active_to_inactive
+
+    print(
+        f"Transition probabilities when inactive: ({inactive_to_inactive / times_inactive}, {inactive_to_active / times_inactive})")
+    print(
+        f"Transition probabilities when active: ({active_to_inactive / times_active}, {active_to_active / times_active})")
+
+    return np.array([[inactive_to_inactive / times_inactive, inactive_to_active / times_inactive],
+                     [active_to_inactive / times_active, active_to_active / times_active]])
+
+
+# Function that executes the utility experiment for a specific deterministic query and DP/BDP queries ("private queries")
+def run_experiment(datasets, non_private_query, private_queries, query_names, legend_loc='best', show_plot=True,
+                   name=None, marker_start_index=0, min_y=None, max_y=None, show_legend=True, **kwargs):
+    resolution = 20
+
+    epsilons = np.linspace(1.0, 20.0, resolution)
+    rmses = np.empty((resolution, len(private_queries)))
+    standard_errors = np.empty((resolution, len(private_queries)))
+    success = np.empty((resolution, len(private_queries))).astype(bool)
+    real_results = non_private_query(datasets)
+    print(f"Average result from deterministic query: {np.mean(real_results)}")
+
+    abs_error_95 = np.empty((resolution, len(private_queries)))
+    lp_scales = np.empty((resolution, len(private_queries)))
+
+    for i, epsilon in tqdm(enumerate(epsilons), total=resolution):
+        for k, private_query in enumerate(private_queries):
+            try:
+                private_results, lp_scales[i, k] = private_query(datasets, epsilon, **kwargs)
+                error_results = private_results - real_results
+
+                # Here we calculate the upper limit of the 95%-confidence interval of absolute errors
+                abs_error_95[i, k] = np.quantile(np.abs(error_results), q=0.95)
+
+                # Here we calculate the squared error
+                squared_errors = error_results ** 2
+                rmses[i, k] = np.sqrt(np.mean(squared_errors))
+                standard_errors[i, k] = np.std(squared_errors, ddof=1) / np.sqrt(np.size(squared_errors))
+                success[i, k] = True
+            except ValueError:
+                success[i, k] = False
+
+    # This is the confidence interval upper limit plot
+    for i in range(len(private_queries)):
+        plt.scatter(epsilons[success[:, i]], abs_error_95[success[:, i], i],
+                     label=query_names[i], color=COLORS[i+marker_start_index], marker=MARKERS[i+marker_start_index], s=100)
+        # Formula to calculate utility of the laplace mechanism based on the scale of the Laplacian distribution
+        alphas = np.log(1 / 0.05) * lp_scales[success[:, i], i]
+        plt.plot(epsilons[success[:, i]], alphas, color=COLORS[i+marker_start_index])
+
+    if show_legend:
+        plt.legend(loc=legend_loc, fontsize=20)
+    plt.xlabel('ε', fontsize=20)
+    plt.yscale('log')
+    plt.ylabel('α', fontsize=20, rotation='horizontal')
+    plt.xticks(fontsize=20, ticks=[2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
+    plt.yticks(fontsize=20)
+    plt.grid(visible=True, axis='both')
+    plt.gca().set_axisbelow(True)
+    if min_y is not None and max_y is not None:
+        plt.ylim((min_y, max_y))
+    if SAVE and name is not None:
+        plt.savefig(f'figures/{name}_confidence_interval.pdf', bbox_inches='tight')
+    if show_plot:
+        plt.show()
+
+    # This is the RMSE plot
+    for i in range(len(private_queries)):
+        plt.scatter(epsilons[success[:, i]], rmses[success[:, i], i],
+                     label=query_names[i], color=COLORS[i+marker_start_index], marker=MARKERS[i+marker_start_index], s=100)
+
+    if show_legend:
+        plt.legend(loc=legend_loc, fontsize=20)
+    plt.xlabel('ε', fontsize=20)
+    plt.yscale('log')
+    plt.ylabel('RMSE', fontsize=20)
+    plt.xticks(fontsize=20, ticks=[2, 4, 6, 8, 10, 12, 14, 16, 18, 20])
+    plt.yticks(fontsize=20)
+    plt.grid(visible=True, axis='both')
+    plt.gca().set_axisbelow(True)
+    if min_y is not None and max_y is not None:
+        plt.ylim((min_y * 0.1, max_y))
+    if SAVE and name is not None:
+        plt.savefig(f'figures/{name}_RMSE.pdf', bbox_inches='tight')
+    if show_plot:
+        plt.show()
+
+
+# Load data and execute experiment for Markov chain data
+def experiment_for_markov_chain():
+    activity_data = pd.read_csv("datasets/activity.csv")
+    activity_data = activity_data.dropna()
+
+    probs = calc_transition_probabilities(activity_data)
+
+    # All states in one dataset
+    np_datasets = np.tile(activity_data['steps'].to_numpy(dtype=int), (1000, 1))
+
+    run_experiment(np_datasets, count_active,
+                   [count_active_bdp_general_bound, count_active_bdp_markov_chain_boun, count_active_dp],
+                   ["", "", ""], show_legend=False, trans_probs=probs, legend_loc='best', min_y=1e-1, max_y=1e5,
+                   name='mse_markov_total_ALT')
+
+    # Split dataframe into many dataframes, one per day
+    data_per_day = [x for _, x in activity_data.groupby(['date'])]
+
+    np_datasets = np.empty((len(data_per_day), len(data_per_day[0])))
+    for i, df in enumerate(data_per_day):
+        np_datasets[i] = df['steps'].to_numpy()
+
+    run_experiment(np_datasets, count_active,
+                   [count_active_bdp_general_bound, count_active_bdp_markov_chain_boun, count_active_dp],
+                   ["General Bound", "Markov Chain Bound", "DP Query"], trans_probs=probs, legend_loc='best', min_y=1e-1,
+                   max_y=1e5, name='mse_markov_per_day_ALT')
+
+
+# Load data and execute experiment for Gaussian data
+def experiment_for_gaussian():
+    height_data = pd.read_stata("datasets/dataverse_files_galton_height/galton.dta")
+    height_data = height_data.dropna()
+    # Shuffle rows
+    height_data = height_data.sample(frac=1, random_state=14)
+
+    # Two versions: (1) One data set with all heights, including the parents. (2) Many data sets with heights of two
+    # parents and their child each.
+    # Use set to remember to only use parents data of each family once.
+    families_already_seen = set()
+    trio_datasets = []
+    full_dataset = []
+    largest_family_size = 0
+    for _, row in height_data.iterrows():
+        trio_datasets.append([row['height'], row['father'], row['mother']])
+        full_dataset.append(row['height'])
+        largest_family_size = max(largest_family_size, row['kids'] + 2)
+        if row['family'] not in families_already_seen:
+            full_dataset.append(row['father'])
+            full_dataset.append(row['mother'])
+            families_already_seen.add(row['family'])
+    trio_datasets = np.array(trio_datasets)
+    full_datasets = np.tile(np.array(full_dataset), (1000, 1))
+    # Calculate maximum empirical Pearson correlation coefficient
+    corr_matrix = np.corrcoef(trio_datasets.T)
+    print("Pearson correlation coefficients")
+    print(corr_matrix)
+    max_rho = np.max([corr_matrix[0,1], corr_matrix[0,2], corr_matrix[1,2]])
+    print(f"Maximum empirical Pearson correlation coefficient: {max_rho}")
+    print(f"Largest family: {largest_family_size} members.")
+
+    run_experiment(trio_datasets, sum, [sum_bdp_general_bound, sum_bdp_gaussian_bound, sum_dp],
+                   ["General bound", "Gaussian bound", "DP Query"], min_value=60, max_value=80, max_rho=max_rho,
+                   name='mse_gaussian_trios')
+    run_experiment(full_datasets, sum, [sum_bdp_general_bound], ["General bound"], min_value=60, max_value=80,
+                   m=largest_family_size, name='mse_gaussian_all')
+
+
+if __name__ == "__main__":
+    SAVE = False
+    experiment_for_markov_chain()
+    experiment_for_gaussian()
+
+
+
